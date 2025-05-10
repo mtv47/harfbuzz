0def6879115ccb589f783f8cc7250ce3acc40dde
Carl Schütz <carl.schutz@epfl.ch>
2025-05-10 11:01:59 +0200

    hb_font_get_var_coords_normalized() is back

--- test/fuzzing/hb-draw-fuzzer.cc
+++ test/fuzzing/hb-draw-fuzzer.cc
@@ -91,7 +91,8 @@ static void var_calls(const uint8_t *data, size_t size, hb_face_t *face,
     unsigned int rand_index = data[0] % instance_count;
     hb_font_set_var_named_instance(font, rand_index);
     hb_font_get_var_named_instance(font);
-    //hb_font_get_var_coords_normalized(font, &numCoords);
+    unsigned int num_coords_norm = numCoords;
+    hb_font_get_var_coords_normalized(font, &num_coords_norm);
     if (size > 8)
     {
       const unsigned int max_tags = 4;

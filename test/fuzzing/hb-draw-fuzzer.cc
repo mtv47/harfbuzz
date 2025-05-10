c765e6be0bb075b726aa4e9fd2cf3dadd9f23d02
Carl Schütz <carl.schutz@epfl.ch>
2025-05-10 16:20:52 +0200

    Test new harness

--- test/fuzzing/hb-draw-fuzzer.cc
+++ test/fuzzing/hb-draw-fuzzer.cc
@@ -150,57 +150,11 @@ static void misc_calls_for_gid (hb_face_t *face, hb_font_t *font, hb_set_t *set,
 
 extern "C" int LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
 {
-  alloc_state = _fuzzing_alloc_state (data, size);
-
-  hb_blob_t *blob = hb_blob_create ((const char *) data, size,
-				    HB_MEMORY_MODE_READONLY, nullptr, nullptr);
-  hb_face_t *face = hb_face_create (blob, 0);
-  hb_font_t *font = hb_font_create (face);
-
-  unsigned num_coords = 0;
-  if (size) num_coords = data[size - 1];
-  num_coords = hb_ot_var_get_axis_count (face) > num_coords ? num_coords : hb_ot_var_get_axis_count (face);
-  int *coords = (int *) calloc (num_coords, sizeof (int));
-  if (size > num_coords + 1)
-    for (unsigned i = 0; i < num_coords; ++i)
-      coords[i] = ((int) data[size - num_coords + i - 1] - 128) * 10;
-  hb_font_set_var_coords_normalized (font, coords, num_coords);
-  var_calls(data, size, face, font, coords, num_coords);
-  free (coords);
-
-  unsigned glyph_count = hb_face_get_glyph_count (face);
-  glyph_count = glyph_count > 16 ? 16 : glyph_count;
-
-  _draw_data_t draw_data = {0, 0, 0, 0, 0};
-
-  hb_draw_funcs_t *funcs = hb_draw_funcs_create ();
-  hb_draw_funcs_set_move_to_func (funcs, (hb_draw_move_to_func_t) _move_to, nullptr, nullptr);
-  hb_draw_funcs_set_line_to_func (funcs, (hb_draw_line_to_func_t) _line_to, nullptr, nullptr);
-  hb_draw_funcs_set_quadratic_to_func (funcs, (hb_draw_quadratic_to_func_t) _quadratic_to, nullptr, nullptr);
-  hb_draw_funcs_set_cubic_to_func (funcs, (hb_draw_cubic_to_func_t) _cubic_to, nullptr, nullptr);
-  hb_draw_funcs_set_close_path_func (funcs, (hb_draw_close_path_func_t) _close_path, nullptr, nullptr);
-  volatile unsigned counter = !glyph_count;
-  hb_set_t *set = hb_set_create ();
-  for (unsigned gid = 0; gid < glyph_count; ++gid)
-  {
-    hb_font_draw_glyph (font, gid, funcs, &draw_data);
-
-    /* Glyph extents also may practices the similar path, call it now that is related */
-    hb_glyph_extents_t extents;
-    if (hb_font_get_glyph_extents (font, gid, &extents))
-      counter += !!extents.width + !!extents.height + !!extents.x_bearing + !!extents.y_bearing;
-
-    if (!counter) counter += 1;
-
-    /* other misc calls */
-    misc_calls_for_gid (face, font, set, gid);
+  if (size >= 3) {
+    // Generate baseline_tag directly from fuzz data (interpreted as HB_TAG)
+    hb_ot_layout_baseline_tag_t tag = static_cast<hb_ot_layout_baseline_tag_t>(
+      HB_TAG(data[0], data[1], data[2], data[3]));
+    hb_ot_tag_to_language(tag);
   }
-  hb_set_destroy (set);
-  assert (counter);
-  hb_draw_funcs_destroy (funcs);
-
-  hb_font_destroy (font);
-  hb_face_destroy (face);
-  hb_blob_destroy (blob);
   return 0;
 }
